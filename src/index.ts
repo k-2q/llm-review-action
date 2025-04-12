3e93a54ea390de9dfeddd10f67c11197e7bdd395
k <42290012+k-2q@users.noreply.github.com>
2025-04-13 00:18:54 +0530

    Added fetch head

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello World!");
+import { setFailed } from "@actions/core";
+import { context } from "@actions/github";
+
+const run = async () => {
+  const pullRequest = context.payload.pull_request;
+
+  try {
+    if (!pullRequest) {
+      throw new Error("This action can only be run on Pull Requests.");
+    }
+
+    console.log("Hello World!");
+
+    const fetchHead = process.env.FETCH_HEAD;
+    const fetchHeadParent = process.env.FETCH_HEAD_PARENT;
+
+    console.log(`FETCH_HEAD: ${fetchHead}`);
+    console.log(`FETCH_HEAD^: ${fetchHeadParent}`);
+  } catch (error) {
+    setFailed((error as Error)?.message ?? "Unknown error");
+  }
+};
+
+run();

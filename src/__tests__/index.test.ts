1e2676e473c0c1bdc8c15df1c26a440ba53148fa
k <42290012+k-2q@users.noreply.github.com>
2025-04-13 17:50:35 +0530

    Add comment

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -3,6 +3,7 @@ import { context } from "@actions/github";
 import { execSync } from "child_process";
 import { jest } from "@jest/globals";
 import { run } from "../index";
+import { readFile } from "fs/promises";
 
 // Mock the dependencies
 jest.mock("@actions/core", () => ({
@@ -26,6 +27,27 @@ describe("run function", () => {
     jest.clearAllMocks();
   });
 
+  it("should execute git diff and log output", async () => {
+    // Arrange
+    context.payload.pull_request = { id: 123 } as any;
+    process.env.FETCH_HEAD = "mock-fetch-head";
+    process.env.FETCH_HEAD_PARENT = "mock-fetch-head-parent";
+
+    const mockDiff = (await readFile(`${__dirname}/mock-diff.txt`)).toString();
+
+    const mockExecSync = execSync as jest.MockedFunction<typeof execSync>;
+    mockExecSync.mockReturnValue(mockDiff);
+
+    // Act
+    await run();
+
+    // Assert
+    expect(mockExecSync).toHaveBeenCalledWith(
+      "git diff mock-fetch-head-parent mock-fetch-head",
+      { encoding: "utf-8" }
+    );
+  });
+
   it("should fail if not triggered by a pull request", async () => {
     // Arrange
     context.payload.pull_request = null as any;
@@ -45,25 +67,6 @@ describe("run function", () => {
 
     // Act
     await run();
-  });
-
-  it("should execute git diff and log output", async () => {
-    // Arrange
-    context.payload.pull_request = { id: 123 } as any;
-    process.env.FETCH_HEAD = "mock-fetch-head";
-    process.env.FETCH_HEAD_PARENT = "mock-fetch-head-parent";
-
-    const mockExecSync = execSync as jest.MockedFunction<typeof execSync>;
-    mockExecSync.mockReturnValue("Mock git diff output");
-
-    // Act
-    await run();
-
-    // Assert
-    expect(mockExecSync).toHaveBeenCalledWith(
-      "git diff mock-fetch-head-parent mock-fetch-head",
-      { encoding: "utf-8" }
-    );
   });
 
   it("should call setFailed on error", async () => {
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { setFailed } from "@actions/core";
-import { context } from "@actions/github";
+import { context, getOctokit } from "@actions/github";
 import { execSync } from "child_process";
 
 export const run = async () => {
   const pullRequest = context.payload.pull_request;
+  const repository = context.payload.repository;
 
   try {
     if (!pullRequest) {
@@ -12,17 +13,45 @@ export const run = async () => {
 
     console.log("Hello World!");
 
+    const ghToken = process.env.GITHUB_TOKEN;
     const fetchHead = process.env.FETCH_HEAD;
     const fetchHeadParent = process.env.FETCH_HEAD_PARENT;
-
-    console.log(`FETCH_HEAD: ${fetchHead}`);
-    console.log(`FETCH_HEAD_PARENT: ${fetchHeadParent}`);
-
     const diffOutput = execSync(`git diff ${fetchHeadParent} ${fetchHead}`, {
       encoding: "utf-8",
     });
-    console.log("Git Diff Output:");
-    console.log(diffOutput);
+
+    const changes = diffOutput.split("diff --git");
+
+    if (!ghToken) {
+      throw new Error("Token is required.");
+    }
+    const octokit = getOctokit(ghToken);
+    const owner = "k-2q";
+    const repo = repository?.name;
+    const pullNumber = pullRequest.number;
+
+    await octokit.request(
+      `POST /repos/${owner}/${repo}/pulls/${pullNumber}/comments`,
+      {
+        owner: owner,
+        repo: repo,
+        pull_number: pullNumber,
+        body: "Great stuff! This is a test comment.",
+        commit_id: "ba2c4b4a93112756efe85c6357f329b609ebee0f",
+        path: "a/src/__tests__/index.test.ts b/src/__tests__/index.test.ts",
+        start_line: 1,
+        start_side: "RIGHT",
+        line: 2,
+        side: "RIGHT",
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      }
+    );
+
+    changes.forEach((change) => {
+      console.log(change);
+    });
   } catch (error) {
     setFailed((error as Error)?.message ?? "Unknown error");
   }
